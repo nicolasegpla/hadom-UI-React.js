fc5691f7eaaf1f48977f611a7ca77d49214cc43c
nicolas <nicolasgpla@gmail.com>
2024-09-01 18:29:38 -0500

    nicolas garcia - 01/9/24 - 18:29pm - calendario, funcionalidad rango de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import './App.css'
+import { Calendar } from './Components/Organisms/Calendar'
 function App() {
+
+  const initialValueEvents = [
+    {id: 1, date: '2024-09-03', title: 'Reunión con el equipo', text: 'hola mundo' },
+    {id: 2, date: '2024-09-15', title: 'Lanzamiento de producto', text: 'hola mundo' },
+    {id: 3, date: '2024-09-20', title: 'Cumpleaños de María', text: 'hola mundo' },
+    {id: 4, date: '2024-09-20', title: 'Cumpleaños de María', text: 'hola mundo' },
+    {id: 5, date: '2024-09-20', title: 'Cumpleaños de María', text: 'hola mundo' },
+    {id: 6, date: '2024-09-20', title: 'Cumpleaños de María', text: 'hola mundo' },
+    {id: 7, date: '2024-09-20', title: 'Cumpleaños de María', text: 'hola mundo' },
+    {id: 8, date: '2024-09-20', title: 'Cumpleaños de María', text: 'hola mundo' },
+    {id: 9, date: '2024-09-03', title: 'Reunión con el equipo', text: 'hola mundo' },
+    {id: 10, date: '2024-09-03', title: 'Reunión con el equipo', text: 'hola mundo' },
+    {id: 11, date: '2024-09-03', title: 'Reunión con el equipo', text: 'hola mundo' },
+    
+  ];
+  const events = [
+    { id: 1, startDate: '2024-09-03', endDate: '2024-09-03', title: 'Reunión con el equipo' },
+    { id: 2, startDate: '2024-09-15', endDate: '2024-09-15', title: 'Lanzamiento de producto' }, // Evento de 3 días
+    { id: 3, startDate: '2024-09-20', endDate: '2024-09-20', title: 'Cumpleaños de María' },
+    { id: 4, startDate: '2024-09-20', endDate: '2024-09-20', title: 'Otra reunión' },
+    { id: 5, startDate: '2024-09-22', endDate: '2024-09-22', title: 'Charla con cliente' }, // Evento de 2 días
+    { id: 6, startDate: '2024-09-25', endDate: '2024-09-25', title: 'Cena de trabajo' },
+    { id: 7, startDate: '2024-09-25', endDate: '2024-09-25', title: 'Cumpleaños de Juan' },
+    { id: 8, startDate: '2024-09-03', endDate: '2024-09-03', title: 'Reunión con el equipo' },
+    { id: 9, startDate: '2024-09-03', endDate: '2024-09-03', title: 'Reunión con el equipo' },
+    { id: 10, startDate: '2024-09-03', endDate: '2024-09-03', title: 'Reunión con el equipo' },
+    { id: 11, startDate: '2024-09-03', endDate: '2024-09-03', title: 'Reunión con el equipo' },
+    { id: 12, startDate: '2024-09-03', endDate: '2024-09-03', title: 'Reunión con el equipo' },
+  ];
 
   return (
     <>
-      <h1 className='bg-red-600'>hola mundo</h1>
-      
+      <Calendar eventsList={events} multiEventProp={false} />
     </>
   )
 }
---
+++ src/Components/Organisms/Calendar/index.jsx
@@ -0,0 +1,132 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline"
+import dayjs from "dayjs"
+import isBetween from 'dayjs/plugin/isBetween'; // Importa el plugin isBetween
+import 'dayjs/locale/es'
+import './calendar.css'
+
+function Calendar({eventsList, multiEventProp}) {
+
+    dayjs.locale('es')
+    dayjs.extend(isBetween); // Registra el plugin isBetween
+
+    const [ currentMonth, setCurrentMonth ] = useState(dayjs()) //en este estado inicial almacenamos el dia actual
+    const [ events, setEvents ] = useState(eventsList)
+    const [ multiEvent, setMultiEvent ] = useState(multiEventProp)
+
+    
+
+    const startDay = currentMonth.startOf('month').startOf('week');
+    const endDay = currentMonth.endOf('month').endOf('week');
+
+    const days = [];
+    let day = startDay;
+
+    while (day <= endDay) {
+    days.push(day);
+    day = day.add(1, 'day');
+    }
+
+    
+    const handlePrevMonth = () => {
+        setCurrentMonth(currentMonth.subtract(1, 'month'));
+      };
+    
+      const handleNextMonth = () => {
+        setCurrentMonth(currentMonth.add(1, 'month'));
+      };
+
+      const today = dayjs();
+    
+      const isDisplayingRealCurrentMonth = currentMonth.isSame(currentMonth, 'month');
+      const isDisplayingRealCurrentDay = currentMonth.isSame(currentMonth, 'month');
+        
+
+      const getEventsForDate = (date) => {
+        return events.filter((event) => {
+            const eventStart = dayjs(event.startDate);
+            const eventEnd = dayjs(event.endDate);
+            return date.isBetween(eventStart, eventEnd, 'day', '[]'); // Compara si la fecha está dentro del rango del evento
+          });
+      };
+      function viewEvents(date) {
+        const eventsForDate = getEventsForDate(date);
+        console.log(eventsForDate);
+      }
+    
+      function totalEventsPerDay(date) {
+        const totalEvents = getEventsForDate(date);
+        if(totalEvents.length > 5) {
+            return 'event-hard'
+        }else if(totalEvents.length > 3) {
+            return 'event-normal'
+        }else if(totalEvents.length > 0 ) {
+            return 'event-slow'
+        }else if(totalEvents.length === 0) {
+            return ''
+        }
+      }
+
+      const isMultiDayEvent = (date) => {
+        return getEventsForDate(date).some((event) => {
+          const eventStart = dayjs(event.startDate);
+          const eventEnd = dayjs(event.endDate);
+          return eventEnd.isAfter(eventStart, 'day'); // Si la fecha de fin es después de la fecha de inicio, es un evento de varios días
+        });
+      };
+      
+
+      const isWithinEventRange = (date) => {
+        return events.some((event) => {
+          const eventStart = dayjs(event.startDate);
+          const eventEnd = dayjs(event.endDate);
+          return eventEnd.isAfter(eventStart, 'day') && date.isBetween(eventStart, eventEnd, 'day', '[]');
+        });
+      };
+
+    return(
+        <>
+            <div className="calendar">
+                <header className="calendar__header">
+                    <button className="calendar__header__button--left" onClick={handlePrevMonth}><ChevronLeftIcon className="calendar__header__button--left__icon" /></button>
+                    <h2 className="calendar__header__h2" >{currentMonth.format('MMMM')}</h2>
+                    <button className="calendar__header__button--right" onClick={handleNextMonth}><ChevronRightIcon className="calendar__header__button--right__icon" /></button>
+                </header>
+                <div className="weekdays">
+                    {['LU', 'MA', 'MI', 'JU', 'VI', 'SA', 'DO'].map((day) => (
+                    <div key={day} className="weekday">
+                        {day}
+                    </div>
+                    ))}
+                </div>
+                <div className="days">
+                {days.map((day) => (
+                    <div
+                        onClick={() => viewEvents(day)}
+                        key={day.format('DD-MM-YYYY')}
+                        className={`day ${
+                        !day.isSame(currentMonth, 'month') ? 'other-month' : ''
+                        } ${
+                            isDisplayingRealCurrentMonth && day.isSame(today, 'week')
+                            ? 'current-week'
+                            : ''
+                        } ${multiEvent && isWithinEventRange(day) ? 'multi-day-event' : '' }`}
+                    >
+                        <div className={`sub-container-day ${
+                            isDisplayingRealCurrentDay && day.isSame(today, 'day')
+                            ? 'current-day'
+                            : ''
+                        } ${totalEventsPerDay(day)}`}>
+                            {day.format('D')}
+                        </div>
+                        
+                    </div>
+                ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export { Calendar }
